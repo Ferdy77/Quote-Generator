3ef87c970d471e09afe269958c80691d18cc868e
Obuluku Ferdinand <fobuluku02@gmail.com>
2025-08-01 13:16:35 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+let quote = document.querySelector(".quote");
+let person = document.querySelector(".person");
+let btn = document.querySelector(".btn")
+
+let quotes = [
+  {
+quote: `"I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best."`,
+  person: `"Marilyn Monroe"`
+}, {
+  quote: `"Two things are infinite: the universe and human     stupidity; and I'm not sure about the universe.""`,
+  person: `"Albert Einstein"`
+}, {
+  quote: `"Be who you are and say what you feel, because those who  mind don't matter, and those who matter don't mind.”`,
+  person: `"Bernard M. Baruch"`
+}, {
+  quote: `"You've gotta dance like there's nobody watching,
+  Love like you'll never be hurt,
+  Sing like there's nobody listening,
+  And live like it's heaven on earth."`,
+  person: `"William W. Purkey"`
+}, {
+  quote: `"You know you're in love when you can't fall asleep because reality is finally better than your dreams."`,
+  person:`"Dr. Seuss"`
+}, {
+  quote: `"If you want to know what a man's like, take a good look at how he treats his inferiors, not his equals."`,
+  person: `"J.K. Rowling, Harry Potter and the Goblet of Fire"`
+},];
+
+btn.addEventListener("click", function() {
+  let random = Math.floor(Math.random() * quotes.length);
+  quote.innerText = quotes[random].quote;
+  person.innerText = quotes[random].person;
+},)
